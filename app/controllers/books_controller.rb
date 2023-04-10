a3ef4e1c685641c822d683880ada3fc066eeb75b
EC2 Default User <ec2-user@ip-172-31-42-118.ap-northeast-1.compute.internal>
2023-04-10 15:28:45 +0000

    change2 heml&css

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -13,23 +13,42 @@ class BooksController < ApplicationController
   end
 
   def new
-    @book = Book.new
+    @book = Book.find(params[:id])
 
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+    if @book.save
+    redirect_to new_book_path(@book.id)
+    flash[:notice] = "successfully"
+    else
+      @books = Book.all
+      render :index
+    end
+
   end
 
   def destroy
     book = Book.find(params[:id])  # データ（レコード）を1件取得
-    book.destroy  # データ（レコード）を削除
+    if book.destroy  # データ（レコード）を削除
+    flash[:notice1] = "successfully destroyed"
+    end
+
+
     redirect_to '/books'  # 投稿一覧画面へリダイレクト
   end
 
   def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+    redirect_to new_book_path(@book.id)
+    flash[:notice] = "successfully"
+    else
+      @books = Book.all
+      render :edit
+    end
+
   end
 
    def book_params
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get 'books/:id/edit' => 'books#edit', as: 'edit_book'
   patch 'books/:id' => 'books#update', as: 'update_book'
   delete 'books/:id' => 'books#destroy', as: 'destroy_book'
+  get 'books/:id/new' => 'books#new', as: 'new_book'
 
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
